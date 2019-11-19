9f49d3f7f4b19701203a15f63b38c3c4723b2951
Romain <r.chevallier21@hotmail.fr>
2019-11-19 01:08:00 +0100

    [Code Review] fix unhandled exception

--- lib/providers/provider_login.dart
+++ lib/providers/provider_login.dart
@@ -3,10 +3,11 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 import 'package:proto_madera_front/providers/http_status.dart';
 
 class ProviderLogin with ChangeNotifier {
-
+  final log = Logger();
   HttpStatus status = HttpStatus.OFFLINE;
   //TODO Externaliser les url dans un autre fichier ?
   // Url localhost Fab
@@ -21,13 +22,18 @@ class ProviderLogin with ChangeNotifier {
   Future<bool> connection(String login, password) async {
     var bytes = utf8.encode(password);
     var digest = sha256.convert(bytes);
-    var response = await http.post(
-      url,
-      headers: {'Content-type': 'application/json'},
-      body: jsonEncode({'login': login, 'password': digest.toString()}),
-    );
+    var response;
+    try {
+      response = await http.post(
+        url,
+        headers: {'Content-type': 'application/json'},
+        body: jsonEncode({'login': login, 'password': digest.toString()}),
+      );
+    } catch (e) {
+      log.e("Error when tryiing to connect:\n" + e.toString());
+    }
     //TODO Ajouter au logger ?
-    if (response.statusCode == 200 && response.body != 'false') {
+    if (response?.statusCode == 200 && response.body != 'false') {
       this.status = HttpStatus.ONLINE;
       return true;
     }
