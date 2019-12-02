cbc8a5504f2a0bcbecc4886d4c0f40176af416bf
Romain <r.chevallier21@hotmail.fr>
2019-12-02 23:34:49 +0100

    [v0.2] Oops !

--- lib/providers/provider_login.dart
+++ lib/providers/provider_login.dart
@@ -12,7 +12,7 @@ class ProviderLogin with ChangeNotifier {
   //TODO Externaliser les url dans un autre fichier ?
   // Url localhost Fab
   var url = "http://10.0.2.2:8081/madera";
-  // var url = "https://google.com";
+  // var url = "https://cesi-madera.fr/madera";
 
   HttpStatus get getStatus => status ??= HttpStatus.OFFLINE;
 
