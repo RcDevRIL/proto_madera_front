cd19942d6c76c75b6f16e547074a04fe72dabb89
Fabien <ladouce.fabien@gmail.com>
2019-12-09 23:06:03 +0100

    [SynchroData] - Création des tables Adresse, Client, ClientAdresse, Projet, ProjetModule
    - Ajout des contraintes !

---
+++ lib/database/tables/projet.dart
@@ -0,0 +1,21 @@
+
+import 'package:moor_flutter/moor_flutter.dart';
+
+class Projet extends Table {
+  IntColumn get projetId => integer().autoIncrement()();
+
+  @override
+  Set<Column> get primaryKey => {projetId};
+
+  TextColumn get nomProjet => text().withLength(min: 0, max: 50)();
+
+  TextColumn get refProjet => text().withLength(min: 0, max: 45)();
+
+  DateTimeColumn get dateProjet => dateTime()();
+
+  RealColumn get prix => real()();
+
+  IntColumn get clientId => integer()();
+
+  IntColumn get devisEtatId => integer()();
+}
