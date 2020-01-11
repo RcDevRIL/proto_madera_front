46ee454f9cf5493076ac94ffe6925ee6b55dc283
BoiteSphinx <boite.sphinx@gmail.com>
2020-01-11 20:40:39 +0100

    [flutter_driver] Test Drawer
    - Added keys on all drawer's buttons
    - Added key parameter on CollapsingListTile and NavigationModel
    - Included flutter_driver test, after "Connection OK" test, it will tap on each drawer's tile, and wait 3 seconds before tapping another one, and then, disconnect.

--- test_driver/madera_app_test.dart
+++ test_driver/madera_app_test.dart
@@ -9,10 +9,23 @@ void main() {
     final usernameTextFinder = find.byValueKey('username');
     final passwordTextFinder = find.byValueKey('password');
     final submitButton = find.byValueKey('connect');
-    final homePage = find.byType('HomePage');
     final authPage = find.byType('AuthenticationPage');
     final alertDialog = find.byType('AlertDialog');
     final alertDialogOkButton = find.byValueKey('ok-button');
+    final profileTile = find.byValueKey('profile-tile');
+    final profilePage = find.byType('ProfilePage');
+    final homeTile = find.byValueKey('home-tile');
+    final homePage = find.byType('HomePage');
+    final quoteToolTile = find.byValueKey('quote_tool-tile');
+    final quoteToolPage = find.byType('QuoteCreation');
+    final quoteOverviewTile = find.byValueKey('quote_overview-tile');
+    final quoteOverviewPage = find.byType('QuoteOverview');
+    final notificationsTile = find.byValueKey('notifications-tile');
+    final notificationsPage = find.byType('NotificationPage');
+    final settingsTile = find.byValueKey('settings-tile');
+    final settingsPage = find.byType('settingsPage');
+    final expandDrawerButton = find.byValueKey('expand-drawer');
+    final logoutButton = find.byValueKey('logout-button');
 
     FlutterDriver driver;
 
@@ -72,6 +85,46 @@ void main() {
             .timeout(Duration(seconds: 12));
       } on Exception catch (e) {
         log.e('Device not connected to internet?\n$e');
+        assert(false);
+      }
+    });
+
+    test('Drawer test OK', () async {
+      await driver.waitFor(homePage).timeout(Duration(seconds: 12));
+
+      await driver.tap(profileTile);
+      await Future.delayed(Duration(seconds: 3));
+      assert(profilePage != null);
+
+      await driver.tap(homeTile);
+      await Future.delayed(Duration(seconds: 3));
+      assert(homePage != null);
+
+      await driver.tap(quoteToolTile);
+      await Future.delayed(Duration(seconds: 3));
+      assert(quoteToolPage != null);
+
+      await driver.tap(quoteOverviewTile);
+      await Future.delayed(Duration(seconds: 3));
+      assert(quoteOverviewPage != null);
+
+      await driver.tap(notificationsTile);
+      await Future.delayed(Duration(seconds: 3));
+      assert(notificationsPage != null);
+
+      await driver.tap(settingsTile);
+      await Future.delayed(Duration(seconds: 3));
+      assert(settingsPage != null);
+
+      try {
+        await driver.tap(expandDrawerButton);
+        await Future.delayed(Duration(seconds: 2));
+
+        await driver.tap(logoutButton);
+        await Future.delayed(Duration(seconds: 3));
+        assert(authPage != null);
+      } catch (e) {
+        log.e("Can't expand drawer... \n $e");
         assert(false);
       }
     });
