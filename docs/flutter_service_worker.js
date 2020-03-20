0a91fa50847503a5eca1377fe337d3f451c75b5c
Romain <r.chevallier21@hotmail.fr>
2020-03-20 15:03:46 +0100

    publish new site with auth deactivated

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1,18 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "47ea47501e0319341b0e5c829517acc5",
-"/assets\assets\img\creationDevis.jpg": "fd90aab727c7be0c295e4663f4a39073",
-"/assets\assets\img\echeancier.PNG": "7a7a231ea631441642659786821b89ab",
-"/assets\assets\img\icons\off.png": "a2079956fec29d93955c27205b0040d9",
-"/assets\assets\img\logo-madera.png": "7c131e087d495f781c644a5c82677549",
-"/assets\assets\img\madera.JPG": "2e097e80c74503a0f755657aec25885e",
-"/assets\assets\img\suiviDevis.jpg": "b0edd81135a80f9ddd28042efab83591",
-"/assets\FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "e5c991351f6273db012653704c3f7073",
+  "/assets/AssetManifest.json": "47ea47501e0319341b0e5c829517acc5",
+"/assets/assets/img/creationDevis.jpg": "fd90aab727c7be0c295e4663f4a39073",
+"/assets/assets/img/echeancier.PNG": "7a7a231ea631441642659786821b89ab",
+"/assets/assets/img/icons/off.png": "a2079956fec29d93955c27205b0040d9",
+"/assets/assets/img/logo-madera.png": "7c131e087d495f781c644a5c82677549",
+"/assets/assets/img/madera.JPG": "2e097e80c74503a0f755657aec25885e",
+"/assets/assets/img/suiviDevis.jpg": "b0edd81135a80f9ddd28042efab83591",
+"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/LICENSE": "99d8919da7431d48f69174640e025dfe",
 "/index.html": "6e2f7e56ade7db26569dd94033a92625",
-"/main.dart.js": "1b0d2e8f83f1f5a41a69412d6bacdc56",
+"/main.dart.js": "d819b2e76a500572aee9dd4a223eb1a7",
 "/sql-wasm.js": "38afbefcc29a055208149c373c93f6f9",
 "/sql-wasm.wasm": "b01552bc79c0b957d4228839bb9b74bf"
 };
@@ -36,9 +36,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
--- lib/ui/pages/common/authentication_page.dart
+++ lib/ui/pages/common/authentication_page.dart
@@ -4,7 +4,8 @@ import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
 import 'package:proto_madera_front/data/blocs/authentication/login_form_bloc.dart';
-import 'package:proto_madera_front/ui/pages/pages.dart' show DecisionPage;
+import 'package:proto_madera_front/ui/pages/pages.dart'
+    show DecisionPage, HomePage;
 import 'package:proto_madera_front/ui/widgets/custom_widgets.dart'
     show AppBarMadera, LabelledIcon, MaderaButton, MaderaRoundedBox;
 import 'package:proto_madera_front/data/providers/providers.dart'
@@ -212,9 +213,9 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
               key: Key('connect'),
               onPressed: snapshot.hasData && snapshot.data == true
                   ? () {
-                     // submit();
-                       Provider.of<MaderaNav>(context)
-                           .redirectToPage(context, HomePage());
+                      // submit();
+                      Provider.of<MaderaNav>(context)
+                          .redirectToPage(context, HomePage(), null);
                     }
                   : null,
               child: Text('Connexion'),
